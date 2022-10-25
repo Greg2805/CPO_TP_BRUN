3d70f3e4d244288469d34f3ff43ab60e6249ec38
Gregoire Brun <gregoire.brun@epfedu.fr>
2022-10-25 11:16:12 +0200

    TP2 ex 2 mise en place du menu

--- TP2_convertisseurBRUN/src/tp2_convertisseurbrun/TP2_convertisseurBRUN.java
+++ TP2_convertisseurBRUN/src/tp2_convertisseurbrun/TP2_convertisseurBRUN.java
@@ -4,6 +4,8 @@
  */
 package tp2_convertisseurbrun;
 
+import java.util.Scanner;
+
 /**
  *
  * @author grego
@@ -15,6 +17,37 @@ public class TP2_convertisseurBRUN {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Scanner sc;
+        sc = new Scanner(System.in);
+        
+        Convertisseur temperature = new Convertisseur();
+        
+        System.out.println("Entrez l'unité de votre température en entrée: ");
+        int a = sc.nextInt ();
+        
+        double result = temperature.CelciusVersKelvin (a);
+        System.out.println(result);
+        
+       
+        double result1 = temperature.KelvinVersCelcius (b);
+        System.out.println(result1);
+        
+        double result2 = temperature.FerenheitVersKelvin (40);
+        System.out.println(result2);
+        
+        double result3 = temperature.KelvinVersFerenheit (40);
+        System.out.println(result3);
+        
+        double result4 = temperature.CelciusVersFerenheit (40);
+        System.out.println(result4);
+        
+        double result5 = temperature.FerenheitVersCelcius (40);
+        System.out.println(result5);
+        
+    
+        
+        
     }
     
 }
