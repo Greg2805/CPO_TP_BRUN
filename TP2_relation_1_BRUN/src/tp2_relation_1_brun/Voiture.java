5cb7ccd95f84c9f404b20d00d2388fc437c3ad14
Gregoire Brun <gregoire.brun@epfedu.fr>
2022-10-25 12:17:20 +0200

    TP2 debut ex 4

---
+++ TP2_relation_1_BRUN/src/tp2_relation_1_brun/Voiture.java
@@ -0,0 +1,14 @@
+package tp2_relation_1_brun;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author grego
+ */
+public class Voiture {
+    
+}
