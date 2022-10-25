d56cda0b9c3e8e41caf4de4ec7ab58d046e00e97
Gregoire Brun <gregoire.brun@epfedu.fr>
2022-10-25 12:12:21 +0200

    TP2 fin ex 3

--- TP2_manip_BRUN/src/tp2_manip_brun/Moussaka.java
+++ TP2_manip_BRUN/src/tp2_manip_brun/Moussaka.java
@@ -1,3 +1,5 @@
+package tp2_manip_brun;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +10,10 @@
  * @author grego
  */
 public class Moussaka {
+    int nbCalories;
+    
+    public Moussaka (){
+        nbCalories=0;
+    }
     
 }
--- TP2_manip_BRUN/src/tp2_manip_brun/Tartiflette.java
+++ TP2_manip_BRUN/src/tp2_manip_brun/Tartiflette.java
@@ -1,3 +1,5 @@
+package tp2_manip_brun;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +10,19 @@
  * @author grego
  */
 public class Tartiflette {
+    int nbCalories;
+    
+    public Tartiflette(){
+        nbCalories=0;
+    }
+    
+    
+    public Tartiflette (int nombres){
+        nbCalories = nombres;
+    }
+    //ce sont les mêmes Tartiflettes
+    
+    
     
 }
+
