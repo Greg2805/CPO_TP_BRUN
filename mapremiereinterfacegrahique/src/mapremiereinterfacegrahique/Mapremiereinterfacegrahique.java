66b1836f8140bd47594ff2acb282e77e8bac2f28
Gregoire Brun <gregoire.brun@epfedu.fr>
2022-11-07 11:15:49 +0100

    création interface projet lasagne ca fonctionne 2

--- mapremiereinterfacegrahique/src/mapremiereinterfacegrahique/Mapremiereinterfacegrahique.java
+++ mapremiereinterfacegrahique/src/mapremiereinterfacegrahique/Mapremiereinterfacegrahique.java
@@ -13,8 +13,6 @@ public class Mapremiereinterfacegrahique {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    
     
 }
