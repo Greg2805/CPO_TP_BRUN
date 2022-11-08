5e76ad53a1646d9f831bde8157239a053c862df5
Gregoire Brun <gregoire.brun@epfedu.fr>
2022-11-08 16:13:36 +0100

    création tableau dynamique

--- TP3_Classe_et_heritage/src/tp3_classe_et_heritage/arme.java
+++ TP3_Classe_et_heritage/src/tp3_classe_et_heritage/arme.java
@@ -1,5 +1,7 @@
 package tp3_classe_et_heritage;
 
+import java.util.ArrayList;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -17,6 +19,9 @@ public class arme {
     public arme (String n, int i){
         attaque = i;
         nom = n;
+        ArrayList<String> n_arme = new ArrayList<String>();
+        n_arme.add(n);
+        
         
     }
     
